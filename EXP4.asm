dc358f6dd2ccefd796f5ef68c6153aa067633b81
lyonljs <lyon673forljs@gmail.com>
2024-11-24 18:54:36 +0800

    11.24 modified1

--- EXP4.asm
+++ EXP4.asm
@@ -1,22 +1,33 @@
 DSEG    SEGMENT
         QUANCIRCLEY DW 300 DUP(0) ;存y坐标
         SAMENUM DB 300 DUP(0)
-        R DW 100
+        R DW 0
         XORG DW 320
         YORG DW 240
         COLOR EQU 10
 
-        FORMERY DW 0
-        LATTERY DW 0
-        DIRECTION DB 0
+        QUERYRIMFORMATION DB 'Please enter the R (from 1 to 240): '
+        ENDIMFORMATION DB 0DH,0AH,24H
+        RSTRING DB 5,0,5 DUP('$')
+        
         
         BIND1 DW 0
         UPPERBIND DW 0
         BIND2 DW 0
         LOWERBIND DW 0
 
+        LUSQX DW 0
+        RUSQX DW 0
+        RDSQX DW 0
+        LDSQX DW 0
+        LUSQY DW 0
+        RUSQY DW 0
+        RDSQY DW 0
+        LDSQY DW 0
+
         FLAG DB 0
         COLORFLAG DB 0
+        DIRECTION DB 0
 
 DSEG    ENDS
 
@@ -32,7 +43,11 @@ SCREEN MACRO
         INT 10H
 ENDM
 
-
+PRINT MACRO STRING
+        MOV AH,09H
+        LEA DX,STRING
+        INT 21H
+ENDM
 
 CSEG    SEGMENT
         ASSUME  CS:CSEG,DS:DSEG
@@ -41,14 +56,86 @@ BEGIN:
         MOV AX,DSEG
         MOV DS,AX
 
-        SCREEN
 
+        CALL QUERYR
+        SCREEN
+        CMP R, 0
+        JNZ NOTONE
+        MOV AH, 0CH
+        MOV AL, 4
+        MOV CX, XORG
+        MOV DX, YORG
+        INT 10H
+        JMP RETURN
+
+NOTONE:
         CALL CALCULATEY
         CALL WRITECIRCLE
+        CALL WRITESQUARE
+
+RETURN:
+        MOV AH, 2        ; BIOS 功能：设置光标位置
+        MOV BH, 0        ; 页号，通常为 0
+        MOV DL, 10          ; X 坐标（列）
+        MOV DH, 10          ; Y 坐标（行）
+        INT 10H            ; 调用 BIOS
+
+        ; 绘制字符
+        MOV AH, 0EH        ; BIOS 功能：显示字符
+        MOV AL, 'H'         ; 显示字符 'H'
+        MOV BL, 0FH        ; 白色前景，黑色背景
+        INT 10H            ; 调用 BIOS
+
+
 
 
         MOV AH,4CH
         INT 21H
+
+QUERYR PROC NEAR
+        PRINT QUERYRIMFORMATION
+        MOV AH, 0AH
+        LEA DX, RSTRING
+        INT 21H
+        PRINT ENDIMFORMATION
+        CALL STORER
+        RET
+QUERYR ENDP
+
+STORER PROC NEAR
+        PUSH CX
+        PUSH BX
+        PUSH DX
+        PUSH AX
+
+        MOV CX, 0
+        MOV CL, RSTRING[1]
+        LEA BX, RSTRING[2]
+        MOV DX, 0
+        PUSH DX
+STORE:
+        MOV AX, 10
+        POP DX
+        MUL DX
+        MOV DX, 0
+        MOV DL, [BX]
+        SUB DL, 30H
+        ADD AX, DX
+        PUSH AX
+        INC BX
+        LOOP STORE
+
+        POP DX
+        DEC DX
+        MOV R, DX
+
+        POP AX
+        POP DX
+        POP BX
+        POP CX
+        RET
+
+STORER ENDP
 
 CALCULATEY PROC NEAR
         PUSH AX
@@ -92,7 +179,7 @@ WRITECIRCLE PROC NEAR
         MOV AH, 0CH
         MOV AL, COLOR
         
-
+        
         MOV SI, 0
         MOV DX, XORG
         DEC DX
@@ -110,19 +197,11 @@ WRITECIRCLE PROC NEAR
         MOV LOWERBIND, DX
         MOV DX, 0 ; 行数
 
-        CMP DIRECTION, 0
-        JE LU
-        CMP DIRECTION, 1
-        JE RU
-        CMP DIRECTION, 2
-        JE RD
-        CMP DIRECTION, 3
-        JE LD
-
 
 
 ; 左上方圆
 LU:
+        MOV DIRECTION,1
 
         MOV SI, 0
         MOV CX, XORG ; 列数
@@ -142,8 +221,8 @@ LP1:
 NOTOVERLU:
         PUSH BX
         MOV BX, 0
-        INT 10H
         CALL DELAYANDCOLOR
+        INT 10H
         POP BX
 
         CMP DX, BX ;如果比下一行的值大1个以上，就继续，否则跳出
@@ -159,8 +238,10 @@ OVERLU:
 
 
 
+
 ; 右上方圆
 RU:
+        MOV DIRECTION,2
 
         MOV SI, R
         ADD SI, R
@@ -170,8 +251,8 @@ RU:
         MOV CX, XORG ; 列数
         MOV DX, YORG
         SUB DX, R
-        INT 10H
         CALL DELAYANDCOLOR
+        INT 10H
 
         INC CX
 
@@ -196,8 +277,8 @@ LP2:
 NOTOVERRU:
         PUSH BX
         MOV BX, 0
-        INT 10H
         CALL DELAYANDCOLOR
+        INT 10H
         POP BX
 
         CMP DX, BX ;如果比下一行的值大1个以上，就继续，否则跳出
@@ -217,6 +298,7 @@ OVERRU:
 ; 右下方圆
 
 RD:
+        MOV DIRECTION,3
 
         MOV SI, 0
         MOV CX, XORG ; 列数
@@ -233,13 +315,14 @@ LP3:
         ADD DX, QUANCIRCLEY[SI] ; DX存当前的y坐标
         ADD BX, QUANCIRCLEY[DI] ; BX存当前位置下一个位置的y坐标
 
+
         DEC BX
 
 NOTOVERRD:
         PUSH BX
         MOV BX, 0
-        INT 10H
         CALL DELAYANDCOLOR
+        INT 10H
         POP BX
 
         CMP DX, BX ;如果比下一行的值大1个以上，就继续，否则跳出
@@ -257,6 +340,7 @@ OVERRD:
 ; 左下方圆
 
 LD:
+        MOV DIRECTION,4
 
         MOV SI, R
         ADD SI, R
@@ -267,8 +351,8 @@ LD:
         MOV DX, YORG
         ADD DX, R
         MOV BX, 0
-        INT 10H
         CALL DELAYANDCOLOR
+        INT 10H
         DEC CX
 
 LP4:
@@ -279,22 +363,22 @@ LP4:
         
         ADD DI, 2
         
-        ADD DX, QUANCIRCLEY[SI] ; DX存当前的y坐标
-        ADD BX, QUANCIRCLEY[DI] ; BX存当前位置下一个位置的y坐标
+        ADD BX, QUANCIRCLEY[SI] ; DX存当前的y坐标
+        ADD DX, QUANCIRCLEY[DI] ; BX存当前位置下一个位置的y坐标
         CMP DX, BX
         JZ NOTOVERLD
-        INC DX
+        DEC DX
 
 NOTOVERLD:
         PUSH BX
         MOV BX, 0
-        INT 10H
         CALL DELAYANDCOLOR
+        INT 10H
         POP BX
 
         CMP DX, BX ;如果比下一行的值大1个以上，就继续，否则跳出
-        JGE OVERLD
-        INC DX
+        JBE OVERLD
+        DEC DX
         JMP NOTOVERLD
 OVERLD:
         SUB SI, 2
@@ -331,7 +415,19 @@ OVER:  MOV SI,CX ;平方根放回原堆栈区
         RET
 SQROOT ENDP
 
+; 延时以及改变颜色的函数
 DELAYANDCOLOR PROC NEAR
+        CMP DIRECTION, 3
+        JNZ DXNOTMID
+        CMP DX, YORG
+        JNZ DXNOTMID
+
+        PUSH DX
+        MOV DL, 1
+        SUB DL, COLORFLAG
+        MOV COLORFLAG, DL
+        POP DX
+DXNOTMID:
         PUSH CX
         PUSH DX
         MOV DL, 1
@@ -339,10 +435,12 @@ DELAYANDCOLOR PROC NEAR
         MOV COLORFLAG, DL
         CMP DL, 0
         JZ COLOR1
-        MOV AL, 8
+        MOV AL, 4
+        JMP COLOROVER
 COLOR1: 
         MOV AL, 10
 
+COLOROVER:
         MOV CX, 10000
 TIMEDELAY:
         NOP
@@ -353,6 +451,92 @@ TIMEDELAY:
         RET
 DELAYANDCOLOR ENDP
 
+WRITESQUARE PROC NEAR
+        PUSH DX
+        PUSH BX
+        PUSH CX
+        PUSH SI
+        PUSH AX
+
+        MOV AX, R
+        MUL AX
+        SHR AX,1
+        MOV SI, AX
+        POP AX
+        CALL SQROOT
+
+        MOV DX, XORG
+        SUB DX, SI
+        MOV LUSQX, DX
+        MOV DX, YORG
+        SUB DX, SI
+        MOV LUSQY, DX
+
+        MOV DX, XORG
+        ADD DX, SI
+        MOV RUSQX, DX
+        MOV DX, YORG
+        SUB DX, SI
+        MOV RUSQY, DX
+
+        MOV DX, XORG
+        ADD DX, SI
+        MOV RDSQX, DX
+        MOV DX, YORG
+        ADD DX, SI
+        MOV RDSQY, DX
+
+        MOV DX, XORG
+        SUB DX, SI
+        MOV LDSQX, DX
+        MOV DX, YORG
+        ADD DX, SI
+        MOV LDSQY, DX
+
+
+        MOV CX, LUSQX
+        MOV DX, LUSQY
+UP:
+        CALL DELAYANDCOLOR
+        INT 10H
+        CMP CX, RUSQX
+        JZ RIGHT
+        INC CX
+        JMP UP
+
+RIGHT:
+        CALL DELAYANDCOLOR
+        INT 10H
+        CMP DX, RDSQY
+        JZ DOWN
+        INC DX
+        JMP RIGHT
+
+DOWN:
+        CALL DELAYANDCOLOR
+        INT 10H
+        CMP CX, LDSQX
+        JZ LEFT
+        DEC CX
+        JMP DOWN
+
+LEFT:
+        CMP DX, LUSQY
+        JZ SQOVER
+        CALL DELAYANDCOLOR
+        INT 10H
+        DEC DX
+        JMP LEFT
+
+SQOVER:
+
+        POP SI
+        POP CX
+        POP BX
+        POP DX
+
+        RET
+WRITESQUARE ENDP
     
 CSEG    ENDS
         END  BEGIN
---
+++ GRAPHletter.asm
@@ -0,0 +1,38 @@
+DSEG    SEGMENT
+       
+DSEG    ENDS
+
+SSEG    SEGMENT PARA STACK 
+        DW  256 DUP(?)
+SSEG    ENDS
+
+SCREEN MACRO
+        MOV AX,12H
+        INT 10H
+        MOV AH,0BH
+        MOV BX,0
+        INT 10H
+ENDM
+
+CSEG    SEGMENT
+        ASSUME  CS:CSEG,DS:DSEG
+.386
+BEGIN:  MOV AX,DSEG
+        MOV DS,AX
+
+        SCREEN
+
+        MOV AH, 2        ; BIOS 功能：设置光标位置
+        MOV BH, 0        ; 页号，通常为 0
+        MOV DL, 10          ; X 坐标（列）
+        MOV DH, 10          ; Y 坐标（行）
+        INT 10H            ; 调用 BIOS
+
+        ; 绘制字符
+        MOV AH, 0EH        ; BIOS 功能：显示字符
+        MOV AL, 'H'         ; 显示字符 'H'
+        MOV BL, 0FH        ; 白色前景，黑色背景
+        INT 10H            ; 调用 BIOS
+
+CSEG    ENDS
+        END  BEGIN
